ccfc0bb53b9eb19daab70122d7d4035f0f4723d5
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-05-14 11:27:53 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get('/' ,(req,res) =>
+{
+    res.send('Hello from Movie Storm')
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log('Time to rent movies on port ' + PORT)
+})
