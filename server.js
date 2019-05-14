7a5cedacbdf158c6d38dd917245a12e47708fbdd
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-05-14 12:44:19 -0400

    add client

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(`${__dirname}/client/build`))
 
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 app.get('/' ,(req,res) =>
 {
