8521b75a8766eec08e31d1224a6e681d7cf40bd6
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-05-14 14:44:25 -0400

    made change to server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/client/build`))
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`)
 })
 
