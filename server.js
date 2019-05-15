8056259a7f8cb710974a1c1ede9bab8955506d9c
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-05-15 17:18:36 -0400

    ...

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/client/build`))
 
-app.use('/', routes)
+
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`)
