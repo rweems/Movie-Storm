49aa8ad9d96e118f88bb0fa5571e419b36f91f5b
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-05-15 17:14:30 -0400

    ...

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ require("dotenv").config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const routes = require('./routes/index.js')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/client/build`))
+
+app.use('/', routes)
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`)
