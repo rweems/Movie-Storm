5a12a95588cee92e0a698ef813691d264cbc5a44
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-05-16 10:12:28 -0400

    added home and about links

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,22 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Users from './components/Users.js'
 import User from './components/User.js'
 import Movie from './components/Movie.js'
+import About from './components/About.js'
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <header>
+            <Link to='/' style={{ textDecoration: 'none', color:'black', paddingRight:'5px' }}>Home</Link> | 
+            <Link to='/about' style={{ textDecoration: 'none', color:'black', paddingLeft:'10px'}}>About</Link>
+          </header>
           <Switch>
             <Route exact path='/' component={Users} />
+            <Route path='/about' component={About} />
             <Route path='/:id' component={User} />
             <Route path='/:id/movie/:movieId' component={Movie} />
           </Switch>
@@ -20,5 +27,7 @@ class App extends Component {
     );
   }
 }
+
+
 
 export default App;
