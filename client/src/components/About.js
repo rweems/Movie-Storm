4e01acda9898e2c129deb08b72bbed853d25c6b3
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-05-16 11:40:01 -0400

    updated about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react'
 
 class About extends Component {
-    state = {  }
     render() { 
         return ( 
-            <div>
+            <div className="text">
                 <h1>About</h1>
                 <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a laoreet dui. Maecenas aliquet malesuada ipsum et 
+                <span>Lorem</span> ipsum dolor sit amet, consectetur adipiscing elit. Nulla a laoreet dui. Maecenas aliquet malesuada ipsum et 
                 malesuada. Proin euismod diam sapien, id blandit massa pharetra id. Pellentesque consequat nisi libero, sed dignissim 
                 quam mollis eget. Duis efficitur orci ligula, quis vulputate elit posuere vitae. Donec accumsan sem dui, vel imperdiet 
                 massa dictum sed. Etiam finibus sem sed finibus cursus. Aliquam commodo elit sed luctus malesuada. Cras vehicula laoreet 
                 nisi vel consequat.</p>
 
                 <p>
-                Vivamus sit amet arcu orci. Nunc ornare enim nisi, at rutrum purus blandit sed. Proin at dolor eu orci volutpat 
+                <span>Vivamus</span> sit amet arcu orci. Nunc ornare enim nisi, at rutrum purus blandit sed. Proin at dolor eu orci volutpat 
                 tristique sed ac ex. Phasellus tristique dui eu magna interdum, eu placerat tellus condimentum. Fusce ullamcorper 
                 odio eget turpis maximus sodales. Vestibulum et diam eget lorem finibus vestibulum eget eu mauris. Donec a ex eros. 
                 Donec commodo elementum purus, eu rutrum libero ullamcorper vitae. Nunc sit amet nisi risus. Etiam vestibulum a 
@@ -22,7 +21,7 @@ class About extends Component {
                 erat convallis eget. Curabitur at aliquam ligula. Donec vel ante semper, commodo neque et, sagittis diam.</p>
 
                 <p>
-                Maecenas facilisis lacinia arcu, sit amet euismod tortor molestie aliquet. Phasellus accumsan lorem vitae est suscipit 
+                <span>Maecenas</span> facilisis lacinia arcu, sit amet euismod tortor molestie aliquet. Phasellus accumsan lorem vitae est suscipit 
                 tincidunt. Sed ornare, ante in aliquet pharetra, enim metus posuere neque, vel aliquam metus nibh et libero. Aliquam 
                 feugiat a lacus id elementum. Donec cursus mi in enim posuere blandit. Aliquam faucibus lorem vel aliquam pretium. 
                 Suspendisse id pulvinar nulla, vitae maximus est. Nam sed justo dui. Fusce tempor, mi at aliquam porttitor, odio purus 
