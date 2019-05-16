720ccf9c3bd3b0cab66acc71b95dbcf005148a05
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-05-16 18:41:52 -0400

    update routes

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ const router = express.Router()
 
 const UserController = require('../controllers/UserController.js')
 
-router.get('/',UserController.index)
-router.post('/',UserController.create)
-router.get('/:id',UserController.show)
-router.put('/:id',UserController.update)
-router.delete('/:id', UserController.delete)
+router.get('/', UserController.index)
+router.post('/', UserController.create)
+router.get('/:id', UserController.show)
+router.put('/:id', UserController.update)
+router.delete('/:id/delete', UserController.delete)
 
 
-module.exports.router
+module.exports = router
