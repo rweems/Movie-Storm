554a2520dc59e8a22f890797b93ca6e24de51322
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-05-16 10:13:15 -0400

    added about page

---
+++ client/src/components/About.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>About</h1>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a laoreet dui. Maecenas aliquet malesuada ipsum et 
+                malesuada. Proin euismod diam sapien, id blandit massa pharetra id. Pellentesque consequat nisi libero, sed dignissim 
+                quam mollis eget. Duis efficitur orci ligula, quis vulputate elit posuere vitae. Donec accumsan sem dui, vel imperdiet 
+                massa dictum sed. Etiam finibus sem sed finibus cursus. Aliquam commodo elit sed luctus malesuada. Cras vehicula laoreet 
+                nisi vel consequat.</p>
+
+                <p>
+                Vivamus sit amet arcu orci. Nunc ornare enim nisi, at rutrum purus blandit sed. Proin at dolor eu orci volutpat 
+                tristique sed ac ex. Phasellus tristique dui eu magna interdum, eu placerat tellus condimentum. Fusce ullamcorper 
+                odio eget turpis maximus sodales. Vestibulum et diam eget lorem finibus vestibulum eget eu mauris. Donec a ex eros. 
+                Donec commodo elementum purus, eu rutrum libero ullamcorper vitae. Nunc sit amet nisi risus. Etiam vestibulum a 
+                ligula convallis sollicitudin. Vestibulum in suscipit metus, quis cursus diam. In tristique orci purus, id ultricies 
+                erat convallis eget. Curabitur at aliquam ligula. Donec vel ante semper, commodo neque et, sagittis diam.</p>
+
+                <p>
+                Maecenas facilisis lacinia arcu, sit amet euismod tortor molestie aliquet. Phasellus accumsan lorem vitae est suscipit 
+                tincidunt. Sed ornare, ante in aliquet pharetra, enim metus posuere neque, vel aliquam metus nibh et libero. Aliquam 
+                feugiat a lacus id elementum. Donec cursus mi in enim posuere blandit. Aliquam faucibus lorem vel aliquam pretium. 
+                Suspendisse id pulvinar nulla, vitae maximus est. Nam sed justo dui. Fusce tempor, mi at aliquam porttitor, odio purus 
+                finibus lorem, vel finibus justo sapien eget mauris. Etiam gravida quis risus in laoreet. Etiam rutrum elit vel suscipit 
+                finibus.
+                </p>
+            </div>
+         );
+    }
+}
+ 
+export default About;
