a86088a7ac3de14df8c9a7981a412506f6079b7f
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-05-17 10:36:24 -0400

    changed seeds

--- db/seeds.js
+++ db/seeds.js
@@ -3,18 +3,38 @@ const User = require('../models/User.js')
 
 const Movie = require('../models/Movie.js')
 
-User.deleteMany().then(()=>{
-    const rickey = new User({name:'Rickey',memberSince:new Date('June 01, 2000'), email:'weems505@gmail.com'})
-    return rickey.save()
-}).then(()=> {
-    const ken = new User({name:'Ken',memberSince:new Date ('November 29, 1999'), email:'kw@gmail.com'})
-    return ken.save()
-})
 
-Movie.deleteMany().then(() =>{
-    const petSemetary = new Movie({title:'Pet Semetary',director:'Kevin Kolsch',genre:'Horror',releaseDate:new Date('April 05, 2019')})
-    return petSemetary.save()
-}).then(()=> {
-    const longShot = new Movie({title:'Long Shot',director:'Jonathan Levine',genre:'Comedy',releaseDate: new Date('May 03, 2019')})
-    return longShot.save()
-})
+let newUsers = [{
+    name: 'Rickey',
+    memberSince: new Date('June 01, 2000'),
+    email: 'weems505@gmail.com'
+},
+{
+    name: 'Ken',
+    memberSince: new Date('November 29, 1999'),
+    email: 'kw@gmail.com'
+}]
+
+let newMovies = [{
+    title: 'Pet Semetary',
+    director: 'Kevin Kolsch',
+    genre: 'Horror',
+    releaseDate: new Date('April 05, 2019')
+},
+{
+    title: 'Long Shot',
+    director: 'Jonathan Levine',
+    genre: 'Comedy',
+    releaseDate: new Date('May 03, 2019')
+}
+]
+
+User.create(newUsers)
+    .then(users => {
+        console.log('Users', users)
+    })
+
+Movie.create(newMovies)
+    .then(movie => {
+        console.log('Movie', movie)
+    })
