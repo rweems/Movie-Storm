9e1efe948bd913813f73506b595ecb4d5917d1bf
Rickey Weems <rickeyweems@Rickeys-MacBook-Pro.local>
2019-05-20 09:55:00 -0400

    added link to movies

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Users from './components/Users.js'
 import User from './components/User.js'
+import Movies from './components/Movies.js'
 import Movie from './components/Movie.js'
 import About from './components/About.js'
 import './App.css';
@@ -14,13 +15,18 @@ class App extends Component {
         <div className='App'>
           <header>
             <Link to='/' style={{ textDecoration: 'none', color:'#336B87', paddingRight:'5px', fontWeight:'bold', fontSize:'20px'}}>Home</Link> | 
-            <Link to='/about' style={{ textDecoration: 'none', color:'#336B87', paddingLeft:'10px', fontWeight:'bold', fontSize:'20px'}}>About</Link>
+            <Link to ='/movie' style={{ textDecoration: 'none', color:'#336B87', paddingLeft:'10px', fontWeight:'bold', fontSize:'20px'}}>Movies</Link> |
+            <Link to='/about' style={{ textDecoration: 'none', color:'#336B87', paddingLeft:'10px', fontWeight:'bold', fontSize:'20px'}}>About</Link> 
+            <h1 className='heading'>Movie-Storm</h1>
+            
+
           </header>
           <Switch>
             <Route exact path='/' component={Users} />
             <Route exact path='/about' component={About} />
             <Route exact path='/user/:id' component={User} />
-            <Route exact path='/:id/movie/:movieId' component={Movie} />
+            <Route exact path='/movie' component={Movies} />
+            <Route exact path='/movie/:movieId' component={Movie} />
           </Switch>
         </div>
       </Router>
